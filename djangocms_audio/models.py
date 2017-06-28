b9e03523ed1937775668854fc5fc560093ae2beb
Matas Dailyda <matas@dailyda.com>
2017-06-28 16:06:14 +0300

    fix typo

--- djangocms_audio/models.py
+++ djangocms_audio/models.py
@@ -125,7 +125,7 @@ class AudioFile(CMSPlugin):
     def get_short_description(self):
         if self.text_title:
             return self.text_title
-        else self.audio_file_id and self.audio_file.label:
+        elif self.audio_file_id and self.audio_file.label:
             return self.audio_file.label
         return ugettext('<file is missing>')
 
